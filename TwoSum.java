0266d5148d61250c636246912ced5ecaac6b7e0c
CN-ZhangYue <ZhangYue3040@gmail.com>
2019-03-30 23:59:55 +0800

    Initial commit

---
+++ TwoSum.java
@@ -0,0 +1,26 @@
+package algorithm;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
+
+public class TwoSum {
+	
+	 public Vector<Integer> twoSum(Vector<Integer> nums, int target) {
+	        
+		 Vector<Integer> resultVector = new Vector<Integer>();
+		 Map<Integer,Integer> map = new HashMap<Integer, Integer>();
+		 int length = nums.size();
+		 
+		 for(int i = 0; i<length; i++) {
+			 int complementation = target - nums.get(i);
+			 if(map.containsKey(complementation)) {
+				 resultVector.add(map.get(complementation));
+				 resultVector.add(i);
+				 return resultVector;
+			 }
+			 map.put(nums.get(i),i);
+		 }
+		 return resultVector;
+	   }
+}
